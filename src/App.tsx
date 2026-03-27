1661351229be2c37009af4bebbbff300e6520fe1
busuttilc <callum.busuttil@booking.com>
2026-03-27 16:35:23 +0000

    fix: updated routing to reset password

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,8 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/reset-password/:uid/:token/" element={<ResetPassword />} />
+
+      <Route path="/reset-password/:uid/:token" element={<ResetPassword />} />
 
       <Route
         path="/"
